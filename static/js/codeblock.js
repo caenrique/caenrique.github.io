6fbb20371299bf44f1f0149bc71c8910deff54f9
Cesar Enrique Ramirez <caenrique93@gmail.com>
2026-02-08 16:01:07 +0100

    adjust relative font sizes

--- static/js/codeblock.js
+++ static/js/codeblock.js
@@ -7,9 +7,6 @@ const errorIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16
 const copyIcon = `<svg aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" class="octicon octicon-copy js-clipboard-copy-icon">
     <path d="M0 6.75C0 5.784.784 5 1.75 5h1.5a.75.75 0 0 1 0 1.5h-1.5a.25.25 0 0 0-.25.25v7.5c0 .138.112.25.25.25h7.5a.25.25 0 0 0 .25-.25v-1.5a.75.75 0 0 1 1.5 0v1.5A1.75 1.75 0 0 1 9.25 16h-7.5A1.75 1.75 0 0 1 0 14.25Z"></path><path d="M5 1.75C5 .784 5.784 0 6.75 0h7.5C15.216 0 16 .784 16 1.75v7.5A1.75 1.75 0 0 1 14.25 11h-7.5A1.75 1.75 0 0 1 5 9.25Zm1.75-.25a.25.25 0 0 0-.25.25v7.5c0 .138.112.25.25.25h7.5a.25.25 0 0 0 .25-.25v-7.5a.25.25 0 0 0-.25-.25Z"></path>
 </svg>`
-// const copyIcon2 = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" class="bi bi-clipboard" viewBox="0 0 16 16">
-//                 <path d="M10 1.5a.5.5 0 0 1 .5-.5h2a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2h-9a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h2a.5.5 0 0 1 .5.5V3h3V1.5zM6.5 3V2h3v1h-3zm4 0v1h2a1 1 0 0 0-1-1h-2V3zm-5 0H3a1 1 0 0 0-1 1v11a1 1 0 0 0 1 1h9a1 1 0 0 0 1-1V4a1 1 0 0 0-1-1H5.5V3z"/>
-//             </svg>`;
 
 // Function to change icons after copying
 const changeIcon = (button, isSuccess) => {
@@ -19,59 +16,29 @@ const changeIcon = (button, isSuccess) => {
   }, 2000);
 };
 
-// Function to get code text from tables, skipping line numbers
-const getCodeFromTable = (codeBlock) => {
-  return [...codeBlock.querySelectorAll('tr')]
-    .map(row => row.querySelector('td:last-child')?.innerText ?? '')
-    .join('');
-};
-
-// Function to get code text from non-table blocks
-const getNonTableCode = (codeBlock) => {
+// Function to get text from code blocks
+const getCode = (codeBlock) => {
   return codeBlock.textContent.trim();
 };
 
 document.addEventListener('DOMContentLoaded', function() {
   // Select all `pre` elements containing `code`
-
-  const observer = new IntersectionObserver((entries) => {
-    entries.forEach(entry => {
-      const pre = entry.target.parentNode;
-      const clipboardBtn = pre.querySelector('.clipboard-button');
-      const label = pre.querySelector('.code-label');
-
-      if (clipboardBtn) {
-        // Adjust the position of the clipboard button when the `code` is not fully visible
-        clipboardBtn.style.right = entry.isIntersecting ? '5px' : `-${entry.boundingClientRect.right - pre.clientWidth + 5}px`;
-      }
-
-      if (label) {
-        // Adjust the position of the label similarly
-        label.style.right = entry.isIntersecting ? '0px' : `-${entry.boundingClientRect.right - pre.clientWidth}px`;
-      }
-    });
-  }, {
-    root: null, // observing relative to viewport
-    rootMargin: '0px',
-    threshold: 1.0 // Adjust this to control when the callback fires
-  });
-
   document.querySelectorAll('pre code').forEach(codeBlock => {
     const pre = codeBlock.parentNode;
-    pre.style.position = 'relative'; // Ensure parent `pre` can contain absolute elements
 
     // Create and append the copy button
     const copyBtn = document.createElement('button');
     copyBtn.className = 'clipboard-button';
     copyBtn.innerHTML = copyIcon;
     copyBtn.setAttribute('aria-label', 'Copy code to clipboard');
-    pre.appendChild(copyBtn);
+    const buttonDiv = document.createElement('span');
+    buttonDiv.className = 'clipboard-button-div';
+    buttonDiv.appendChild(copyBtn);
+    pre.appendChild(buttonDiv);
 
     // Attach event listener to copy button
     copyBtn.addEventListener('click', async () => {
-      // Determine if the code is in a table or not
-      const isTable = codeBlock.querySelector('table');
-      const codeToCopy = isTable ? getCodeFromTable(codeBlock) : getNonTableCode(codeBlock);
+      const codeToCopy = getCode(codeBlock);
       try {
         await navigator.clipboard.writeText(codeToCopy);
         changeIcon(copyBtn, true); // Show success icon
@@ -80,28 +47,5 @@ document.addEventListener('DOMContentLoaded', function() {
         changeIcon(copyBtn, false); // Show error icon
       }
     });
-
-    // const langClass = codeBlock.className.match(/language-(\w+)/);
-    const langClass = codeBlock.getAttribute("data-lang")
-    const lang = langClass ? langClass : 'default';
-
-    // Create and append the label
-    const label = document.createElement('span');
-    label.className = 'code-label label-' + lang; // Use the specific language class
-    label.textContent = lang.toUpperCase(); // Display the language as label
-    pre.appendChild(label);
-
-    let ticking = false;
-    pre.addEventListener('scroll', () => {
-      if (!ticking) {
-        window.requestAnimationFrame(() => {
-          copyBtn.style.right = `-${pre.scrollLeft}px`;
-          label.style.right = `-${pre.scrollLeft}px`;
-          ticking = false;
-        });
-        ticking = true;
-      }
-    });
-
   });
 });
